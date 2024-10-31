7f8309fd0ca5eb804bac3123f24839dc7b7933d7
CarlUdo <carl@wbsweden.com>
2024-10-31 11:46:47 +0100

    add-route-for-add-mob
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,11 +3,11 @@ import { mobsRouter } from "./routers/mobs-router";
 
 export const app = express();
 
+app.use(express.json());
+
 app.use("/api/v1/mobs", mobsRouter);
 
-// app.post("/api/v1/mobs/", (req: Request, resp: Response) => {
-//   resp.status(200).json([]);
-// });
+app.post("/api/v1/mobs/", mobsRouter);
 
 // app.get("/api/v1/mobs/:mobId", (req: Request, resp: Response) => {
 //   resp.status(200).json([]);
--- backend/src/routers/mobs-router.ts
+++ backend/src/routers/mobs-router.ts
@@ -1,14 +1,24 @@
 import express, { Request, Response } from "express";
+import { v4 as uuidv4 } from 'uuid';
 
-export const mobsRouter = express.Router();
+
+export const mobsRouter = express.Router(); // Kör vid input time
 
 const mobs = [{
-  name: "Kåda"
+  name: "Kåda", id: "1"
 },
-{name: "Spicy"},
-{name: "Infinågot"}];
+{name: "Spicy", id: "2"},
+{name: "Infinågot", id: "3"}];
 
 mobsRouter.get("/", (req: Request, res: Response) => {
   res.status(200).json(mobs);
+});
+
+mobsRouter.post("/", (req: Request, res: Response) => {
+  const { name } = req.body;
+  const id = uuidv4();
+  mobs.push({name, id});
+
+  res.status(200).location(`/api/v1/mobs/${id}`).json(id);
 });
 
